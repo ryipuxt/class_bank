2f240acde212d8489b8fc530e26043d99877f6f9
ryipuxt <happy201732@gmail.com>
2024-08-05 11:19:35 +0900

    테이블 설계 및 모델링

--- src/main/java/com/tenco/bank/ClassBankApplication.java
+++ src/main/java/com/tenco/bank/ClassBankApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.tenco.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/tenco/bank/ServletInitializer.java
+++ src/main/java/com/tenco/bank/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.tenco.bank;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
